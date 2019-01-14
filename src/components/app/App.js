8c2fa273b4886d6ce4a96f7165881ab4b476d583
Wade Stewart <stew.wade@gmail.com>
2019-01-14 16:37:25 -0500

    fetching exhibtion data

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { API } from '../../config'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      exhibitions: []
+    }
+  }
 
   componentDidMount = () => {
-    fetch('')
+    fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getList&access_token=${API.apiKey}&page=1&per_page=100`)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data)
+        this.setState({ exhibitions: data.exhibitions})
+      })
   }
 
   render() {
+    console.log(this.state.exhibitions)
     return (
       <div>
         <h1>Hello Suckas!</h1>
