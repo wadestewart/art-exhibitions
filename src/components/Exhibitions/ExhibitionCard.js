7fbd3b4d3bb0c7fbc92f22b21599a27d616aaad8
Wade Stewart <stew.wade@gmail.com>
2019-01-26 13:55:25 -0500

    added inline-block to current class in exhibitions for on hover effect, also cursor pointer

--- src/components/Exhibitions/ExhibitionCard.js
+++ src/components/Exhibitions/ExhibitionCard.js
@@ -5,7 +5,7 @@ import './Exhibitions.css'
 function ExhibitionsCard (props) {
     return (
         <div className="exhibition" onClick={props.onExhibitionClick}>
-            <h3 className="current">{props.title}</h3>
+            <h5 className="current">{props.title}</h5>
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import './Header.css'
+
 const Header = (props) => (
-    <nav className="header">
-        <h1 onClick={props.onReload}>Cooper Hewitt</h1>
-    </nav>
+    <div className="navbar-fixed">
+        <nav className="nav-wrapper cyan lighten-2">
+            <div className="container">
+                <div class="valign-wrapper">
+                    <p className="brand-logo right" onClick={props.onReload}>Cooper Hewitt</p>
+                </div>
+            </div>
+        </nav>
+    </div>
 )
 
 export default Header
